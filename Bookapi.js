926787065ec30f2fc23d1e7bbc380c528ce82f6e
WAQAR2211011KHICND <mohammad.waqar@xloopdigital.com>
2022-12-02 18:57:59 +0500

    bit error in code of AC4

--- Bookapi.js
+++ Bookapi.js
@@ -18,10 +18,27 @@
         let ReadingList = {
             
             BookItem : [],
-
             getBooks: function(){
 
-                return [];
+                let tempBooks = [];
+                
+                BookItem.forEach(element => {
+                    let title = element.book.title;
+                    let author = element.book.author;
+                    let length = element.book.length;
+                    let year = element.book.year;
+                    let dateRead = element.dateRead;
+                    let rating = element.rating;
+                    let ratingStar = '';
+                    for (let index = 0; index < rating; index++) {
+                        ratingStar = ratingStar + "*";
+                        
+                    }
+                   
+                    tempBooks.push([`${title} by ${author}, ${length} pages, ${year} read on ${dateRead}, ${ratingStar}`]);
+                });
+
+                return tempBooks;
             },
 
             addBook: function(book, dateRead, rating){
@@ -32,11 +49,26 @@
                 return this.BookItem.length;
             },
 
-            //Because we have use the object pattern instead of class we have to reset the object (readingList)
+            deleteBook: function(title){
 
+                for (let index = 0; index < BookItem.length; index++) {
+
+                    if(title === BookItem.book.title){
+                        delete BookItem[index];
+                        break;
+                    }
+
+                }
+
+            },
+
+
+            //Because we have use the object pattern instead of class we have to reset the object (readingList)
             resetReadingList: function(){
                 this.BookItem = [];
             }
+
+
         };
 
 
--- bookapi.test.js
+++ bookapi.test.js
@@ -58,5 +58,31 @@ describe('Book Api testing',()=>{
     })
 
 
+     // 4.Given that I have a book in my list, when I call removeBook("<title>") 
+     //    with "title" representing the title of my book that I want to delete, 
+     //    then when I call getBooks() the book I deleted should no longer be there.
+
+     test('when I add the first book then I expect 1',()=>{
+        
+        //Arrange
+        let cut =  ReadingList;
+        
+        let book1 = new Book("The Hobbit", "J.R.R. Tolkein", 350, 1937);
+        let book2 = new Book("Alices Adventures in Wonderland", "Lewis Carroll", 544, 1865);
+        cut.addBook(book1, "January 1, 2020", 5);
+        cut.addBook(book2, "May 22, 1992", 4);
+
+        
+        //Act 
+        cut.deleteBook("Alices Adventures in Wonderland");
+        let actualRes = cut.getBooks();
+
+        
+        //Assert
+        expect(book1.title).toBe(actualRes.title);
+        cut.resetReadingList();
+    })
+
+
   
 })
