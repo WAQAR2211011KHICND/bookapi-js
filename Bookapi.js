998adfaa1dd31dea05347dfa45cb5764bd9ca3f8
WAQAR2211011KHICND <mohammad.waqar@xloopdigital.com>
2022-12-02 17:14:18 +0500

    failing test for AC2 is done

--- Bookapi.js
+++ Bookapi.js
@@ -1,12 +1,38 @@
 
-        let Book ;
+        class Book{
+
+            title  = '';
+            author = '';
+            length = 0 ;
+            year   = 0 ;
+
+            constructor(title, author, length, year){
+                this.title = title;
+                this.author = author;
+                this.length = length;
+                this.year = year;
+            }
+        }
+
 
         let ReadingList = {
             
+
+
             getBooks: function(){
 
                 return [];
             }
+
+            addBook: function(){
+
+
+            }
+
+            numberRead: function(){
+                
+            }
+
         };
 
 
--- bookapi.test.js
+++ bookapi.test.js
@@ -18,6 +18,25 @@ describe('Book Api testing',()=>{
 
     })
 
+    // 2. Given that I have an empty list, when I add the first book to my list then 
+    // I expect numberRead to return 1.
 
-    
+    test('when i first visit site it will be empty',()=>{
+        
+        //Arrange
+        let cut =  ReadingList;
+        let book = new Book("The Hobbit", "J.R.R. Tolkein", 350, 1937);
+        cut.addBook(book, "January 1, 2020", 5);
+
+        //Act 
+        let actual = cut.numberRead();
+        
+        //Assert
+        expect(actual).toBe(1);
+
+    })
+
+
+
+
 })
